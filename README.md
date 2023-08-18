d4422de4b779d5a6989b5a88282cfe640b7815f0
Chrismettal <chrismettal@binary-6.de>
2023-08-18 23:32:10 +0200

    Initial commit (bare bones reading possible)

---
+++ README.md
@@ -0,0 +1,60 @@
+# Splive <!-- omit in toc -->
+
+[![PyPI - Version](https://img.shields.io/pypi/v/TinDHL?style=flat-square)](https://pypi.org/project/TinDHL/)
+[![Repo Version](https://img.shields.io/github/v/tag/chrismettal/TinDHL?label=RepoVersion&style=flat-square)](https://github.com/Chrismettal/TinDHL)
+[![PyPI - License](https://img.shields.io/pypi/l/TinDHL?style=flat-square)](https://pypi.org/project/TinDHL/)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/TinDHL?style=flat-square)](https://pypi.org/project/TinDHL/)
+[![Donations: Coffee](https://img.shields.io/badge/donations-Coffee-brown?style=flat-square)](https://github.com/Chrismettal#donations)
+
+This is a work in progress.
+
+For some reason, DHL exported labels print over the entire A4 page, wasting half of your self sticking labels. Additionally, for international orders, an entire second A4 label is used for the CN22/CN23 document.
+
+This tool grabs these two pages, deletes the half of the page that isn't required for anything, and puts both the label and CN22 on the same page for printing. Additionally, it will fill out the current date into the CN22/CN23 date field so you only need to sign it.
+
+**If you like my work please consider [supporting me](https://github.com/Chrismettal#donations)!**
+
+## Installation
+
+### Pypi
+
+Might be pushed to Pypi later idk.
+
+### Local (for development)
+
+- Clone the repo:
+
+`git clone https://github.com/chrismettal/TinDHL`
+
+- Change directory into said cloned repo:
+
+`cd TinDHL`
+
+- Install in "editable" mode:
+
+`pip install -e .`
+
+## Usage
+
+Execute `TinDHL` in the folder containing your downloaded labels fresh from DHL. Alternatively, specify the source and target paths after the command like `TinDHL /path/to/source/pdf/folder /path/to/target/pdf/folder`.
+
+It will automatically detect national and international orders based on the number of pages in the original document and attempt to use as little pages as possible for your orders (First dumping each international order into one PDF page, before using half a page per national order). 
+
+2 files, `TinDHLs_YYYY-MM-DD.pdf` and `TinDHLs_YY-MM-DD.csv` will be created after completion, where the PDF contains all your labels in one file, and the CSV is a list of recipients and their tracking numbers.
+
+## Roadmap
+
+- [x] Consolidate international labels into one page
+- [x] Consolidate 2 national labels into one page
+- [x] Find list of files
+  - [x] Detect national vs international order
+- [x] Append multiple orders into the same file
+- [ ] Optionally shrink parts down to fit on smaller than A5 packages
+
+## Donations
+
+**If you like my work please consider [supporting me](https://github.com/Chrismettal#donations)!**
+
+## License
+
+ <a rel="GPLlicense" href="https://www.gnu.org/licenses/gpl-3.0.html"><img alt="GPLv3" style="border-width:0" src="https://www.gnu.org/graphics/gplv3-or-later.png" /></a><br />This work is licensed under a <a rel="GPLlicense" href="https://www.gnu.org/licenses/gpl-3.0.html">GNU GPLv3 License</a>.
---
+++ tindhl/tindhl.py
@@ -0,0 +1,203 @@
+#############################################################################
+##                              DHLabel                                    ##
+#############################################################################
+# Command line tool to consolidate several DHL labels 
+# for printing with less waste
+
+import os
+import sys
+import argparse
+import configparser
+import csv
+from appdirs import user_config_dir
+
+
+#############################################################################
+##                           Global variables                              ##
+#############################################################################
+input_file          = ""
+output_path         = ""
+
+#############################################################################
+##                               Helpers                                   ##
+#############################################################################
+
+def argset():
+    """
+    Sets command line arguments
+    """
+    global input_file
+    global output_path
+
+    parser = argparse.ArgumentParser(description=
+        "Command line tool to consolidate several DHL labels for printing with less waste")
+
+    # Input path
+    defaultInputFile = os.getcwd() + "/orders.csv"
+    parser.add_argument('input_file', nargs='?', 
+        default= defaultInputFile,
+        help="""
+            Path to input CSV. Uses "orders.csv" in current path by default
+            """)
+
+    # Output path
+    parser.add_argument('output_path', nargs='?', 
+        default=os.getcwd(),
+        help="""
+            Path of output CSV. Will use 
+            current directory if ommited
+            """)
+
+    # Parse args
+    args = parser.parse_args()
+    input_file  = args.input_file
+    output_path = args.output_path
+
+    # Output recognized args
+    print("Using input path:")
+    print(input_file)
+    print("Using output path:")
+    print(output_path)
+    print("---")
+
+    # Argument sanity checks
+    if not os.path.isfile(input_file):
+        print("The input file specified does not exist, exiting")
+        sys.exit()
+    if not os.path.isdir(output_path):
+        print("The output directory specified does not exist, exiting")
+        sys.exit()
+
+
+def convert(config):
+    """
+    Convert existing Tindie order export to DHL import
+    """
+    
+    global input_file
+    global output_path
+
+    # Generate default output header
+    dhlExportHeader = [
+        '',
+        'SEND_NAME1',
+        'SEND_NAME2',
+        'SEND_STREET'
+        'SEND_HOUSENUMBER',
+        'SEND_PLZ',
+        'SEND_CITY',
+        'SEND_COUNTRY',
+        'RECV_NAME1',
+        'RECV_NAME2',
+        'RECV_STREET',
+        'RECV_HOUSENUMBER',
+        'RECV_PLZ',
+        'RECV_CITY',
+        'RECV_COUNTRY',
+        'PRODUCT',
+        'COUPON',
+        'SEND_EMAIL'
+    ]
+
+    # Generate empty list
+    dhlExportData = []
+
+    # Read in Tindie data
+    with open(input_file, newline='') as infile:
+        reader = csv.DictReader(infile, delimiter=',', quotechar='"')
+         
+        tindieExport = [row for row in reader]
+
+
+    # Fill one row of data
+    dhlExportSingleRow = {
+        '': '',
+        'SEND_NAME1':       config['sender']['name'],
+        'SEND_NAME2':       '',
+        'SEND_STREET':      '',
+        'SEND_HOUSENUMBER': '',
+        'SEND_PLZ':         '',
+        'SEND_CITY':        '',
+        'SEND_COUNTRY':     '',
+        'RECV_NAME1':       '',
+        'RECV_NAME2':       '',
+        'RECV_STREET':      '',
+        'RECV_HOUSENUMBER': '',
+        'RECV_PLZ':         '',
+        'RECV_CITY':        '',
+        'RECV_COUNTRY':     '',
+        'PRODUCT':          '',
+        'COUPON':           '',
+        'SEND_EMAIL':       ''
+    }
+
+    # Add data to new shipping row
+    dhlExportData.append(dhlExportSingleRow)
+
+    # Create new output file
+    output_file = os.path.join(output_path, "TinDHL.csv")
+    with open(output_file, 'w') as outfile:
+        writer = csv.DictWriter(outfile, dhlExportHeader, delimiter=',')
+        
+        writer.writeheader()
+        writer.writerows(dhlExportData)
+
+
+    print("---")
+    print("Conversion done")
+    print("---")
+
+#############################################################################
+##                               main()                                    ##
+#############################################################################
+def main():
+    print("--------------------")
+    print("--Starting  TinDHL--")
+    print("--------------------")
+
+    # User config handling
+    config_dir          = user_config_dir("TinDHL")
+    user_config_path    = os.path.join(config_dir, "config.toml")
+    config              = configparser.ConfigParser()
+
+    # Create path to user config
+    if not os.path.exists(config_dir):
+        os.makedirs(config_dir)
+    
+    # Create config file if it doesn't exist
+    if not os.path.isfile(user_config_path):
+        print("""
+            Default config file did not exist. 
+            Please manually enter your default values in the following file:
+            """)
+        print(user_config_path)
+
+        config['sender'] = {'name':             'Danny Default',
+                            'street':           'Rad Rd',
+                            'house number':     '666',
+                            'zipcode':          '12345',
+                            'city':             'Tubular Town',
+                            'country':          'USA',
+                            'email':            'DannyDefault@Radmail.com',}
+
+        with open(user_config_path, 'w') as configfile:
+            config.write(configfile)
+
+        sys.exit()
+
+    # Read existing user config 
+    with open(user_config_path, 'r') as configfile:
+        config.read(user_config_path)
+
+    # Set command line arguments
+    argset()
+
+    # Create spliced file
+    convert(config)
+
+
+#############################################################################
+##                         main() idiom                                    ##
+#############################################################################
+if __name__ == "__main__":
+    main()
